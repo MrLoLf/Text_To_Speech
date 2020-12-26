3065266f4d67acee052e64363641bf8a5312bd6e
MrLoLf <66185550+MrLoLf@users.noreply.github.com>
2020-12-26 19:12:11 +0100

    Add files via upload

--- Text_To_Speech.py
+++ Text_To_Speech.py
@@ -1,25 +1,34 @@
-import gtts
 from gtts import gTTS
 from playsound import playsound
-import tkinter as tk
 import os
-from pathlib import Path
+import winsound
+try:
+    from pathlib import Path
+    import tkinter as tk
+except ImportError:
+    from pathlib2 import Path
+    import Tkinter as tk
 
 
 def main():
     def txt_to_speech():
         try:
             try:
-                message = msg.get()
                 check.set("")
                 lab.config(bg="white")
-                speech = gTTS(text=message, lang=lang.get())
+                speech = gTTS(text=msg.get(), lang=lang.get())
                 speech.save("Text.mp3")
                 __dirpath__ = Path(globals().get("__file__", "./_")).absolute().parent
                 path = str(__dirpath__) + "\\Text.mp3"
-                playsound(path)
+                try:
+                    playsound(path)
+                except Exception:
+                    winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
+                    check.set("Wasn't able to load the characters!")
+                    lab.config(bg="red")
                 os.remove(path)
             except ValueError:
+                winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
                 check.set("The langauge is not supported!")
                 lab.config(bg="red")
         except AssertionError:
@@ -35,94 +44,123 @@ def main():
 
     def ar():
         lang.set("ar")
+        language.set("Language: Arabic")
 
     def bn():
         lang.set("bn")
+        language.set("Language: Bengali")
 
-    def zh_CN():
+    def zh_cn():
         lang.set("zh-CN")
+        language.set("Language: Chinese")
 
     def cs():
         lang.set("cs")
+        language.set("Language: Czech")
 
     def da():
         lang.set("da")
+        language.set("Language: Danish")
 
     def nl():
         lang.set("nl")
+        language.set("Language: Dutch")
 
     def en():
         lang.set("en")
+        language.set("Language: English")
 
     def fil():
         lang.set("fil")
+        language.set("Language: Filipino")
 
     def fi():
         lang.set("fi")
+        language.set("Language: Finnish")
 
     def fr():
         lang.set("fr")
+        language.set("Language: French")
 
     def de():
         lang.set("de")
+        language.set("Language: German")
 
     def el():
         lang.set("el")
+        language.set("Language: Greek")
 
     def hi():
         lang.set("hi")
+        language.set("Language: Hindi")
 
     def hu():
         lang.set("hu")
+        language.set("Language: Hungarian")
 
-    def id():
+    def ind():
         lang.set("id")
+        language.set("Language: Indonesian")
 
     def it():
         lang.set("it")
+        language.set("Language: Italian")
 
     def ja():
         lang.set("ja")
+        language.set("Language: Japanese")
 
     def ko():
         lang.set("ko")
+        language.set("Language: Korean")
 
     def no():
         lang.set("no")
+        language.set("Language: Norwegian")
 
     def pl():
         lang.set("pl")
+        language.set("Language: Polish")
 
     def pt():
         lang.set("pt")
+        language.set("Language: Portuguese")
 
     def ru():
         lang.set("ru")
+        language.set("Language: Russian")
 
     def sk():
         lang.set("sk")
+        language.set("Language: Slovak")
 
     def es():
         lang.set("es")
+        language.set("Language: Spanish")
 
     def sv():
         lang.set("sv")
+        language.set("Language: Swedish")
 
     def th():
         lang.set("th")
+        language.set("Language: Thai")
 
     def tr():
         lang.set("tr")
+        language.set("Language: Turkish")
 
     def uk():
         lang.set("uk")
+        language.set("Language: Ukrainisch")
 
     def vi():
         lang.set("vi")
+        language.set("Language: Vietnamese")
 
     root = tk.Tk()
     root.title("Text To Speech")
-    root.geometry("285x150")
+    root.geometry("300x160")
     lang = tk.StringVar()
     lang.set("en")
 
@@ -132,7 +170,7 @@ def main():
     menu.add_cascade(label="Language", menu=langmenu)
     langmenu.add_command(label="Arabic", command=ar)
     langmenu.add_command(label="Bengali", command=bn)
-    langmenu.add_command(label="Chinese", command=zh_CN)
+    langmenu.add_command(label="Chinese", command=zh_cn)
     langmenu.add_command(label="Czech", command=cs)
     langmenu.add_command(label="Danish", command=da)
     langmenu.add_command(label="Dutch", command=nl)
@@ -144,7 +182,7 @@ def main():
     langmenu.add_command(label="Greek", command=el)
     langmenu.add_command(label="Hindi", command=hi)
     langmenu.add_command(label="Hungarian", command=hu)
-    langmenu.add_command(label="Indonesian", command=id)
+    langmenu.add_command(label="Indonesian", command=ind)
     langmenu.add_command(label="Italian", command=it)
     langmenu.add_command(label="Japanese", command=ja)
     langmenu.add_command(label="Korean", command=ko)
@@ -168,9 +206,13 @@ def main():
     tk.Button(root, text="Play", font="arial 13", bg="lightgreen", command=txt_to_speech).grid(row=3, column=0)
     tk.Button(root, text="Exit", font="arial 13", bg="red", command=kill).grid(row=3, column=1)
     tk.Button(root, text="Reset", font="arial 13", bg="yellow", command=reset).grid(row=3, column=2)
+    language = tk.StringVar()
+    language.set("Language: English")
+    langlab = tk.Label(root, textvariable=language, font="arial 13")
+    langlab.grid(row=4, column=1)
     check = tk.StringVar()
     lab = tk.Label(root, textvariable=check)
-    lab.grid(row=4, column=1)
+    lab.grid(row=5, column=1)
     root.mainloop()
 
 
